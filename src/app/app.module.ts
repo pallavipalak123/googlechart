ade51450089a93d4e192f6909886e7b49e62cf26
pallavipalak123 <pallavi.palak1994@gmail.com>
2017-07-25 20:10:11 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { NgModule }       from '@angular/core';
+import { BrowserModule  } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import {GoogleChart} from '../directives/angular2-google-chart.directive';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent,GoogleChart],
+    imports:      [BrowserModule],
+    bootstrap:    [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
